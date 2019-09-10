e7d0b8f67d309421a4641b6e70a6421f1b7ce4dc
anuradha <saggunasekara@gmail.com>
2019-09-10 17:51:48 +0530

    integrated youtube-dl

--- core/config.js
+++ core/config.js
@@ -1,6 +1,3 @@
 export default {
-  JIRA: {
-      issueUrl: '/rest/api/2/issue'
-  },
-  PORT: 3000
+  PORT: 3400
 };
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import bodyParser from 'body-parser';
 import swaggerJSDoc from 'swagger-jsdoc';
 import swaggerUi from 'swagger-ui-express';
 
-import jiraRoute from './routes/jira.routes';
+import routes from './routes/youtube-dl.routes';
 import config from './core/config';
 import swaggerOptions from './core/swagger';
 
@@ -11,14 +11,13 @@ const swaggerSpec = swaggerJSDoc(swaggerOptions);
 
 const port = config.PORT;
 
-
 const app = express();
 
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.use(bodyParser.json());
 
-app.use('/jira', jiraRoute);
+app.use('/youtube-dl', routes);
 
 app.get('/', (req, res) => {
   res.send('Invalid route');
